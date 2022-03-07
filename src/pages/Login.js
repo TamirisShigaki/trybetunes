9d58ba8026a49e8db9e194eb26657a0adf5f0bf9
Tamiris Shigaki <shigakiendo@gmail.com>
2022-03-07 17:11:32 -0300

    requisito 2: falta chamar a função creatUser e nem redireciona pra /search

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,83 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
+
+const INITIAL_STATE = {
+  name: '',
+  btnLoading: true,
+  isLoading: false,
+  loading: false,
+};
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = INITIAL_STATE;
+  }
+
+  // verifica se o input name tem mais de 3 caráter e habilita o botão.
+  handleChange = (event) => {
+    const min = 3;
+
+    this.setState(() => ({
+      name: event.target.value,
+      btnLoading: event.target.value.length < min,
+    }));
+  }
+
+  btnSubmit = (event) => {
+    event.preventDefault();
+
+    const { name } = this.state;
+    this.setState({
+      loading: true,
+    }, async () => {
+      await createUser({ name });
+      this.setState({
+        loading: false,
+        isLoading: true,
+      });
+    });
+  }
+
   render() {
+    const { btnLoading, isLoading, loading } = this.state;
+
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        {
+          loading ? <Loading /> : (
+            <form>
+              <label htmlFor="name-input">
+                Nome:
+                <input
+                  type="text"
+                  name="name"
+                  id="name-input"
+                  data-testid="login-name-input"
+                  onChange={ (event) => this.handleChange(event) }
+                />
+              </label>
+
+              <button
+                type="submit"
+                name="btnLoading"
+                id="submit-button"
+                data-testid="login-submit-button"
+                onClick={ (event) => this.handleChange(event) }
+                disabled={ btnLoading }
+              >
+                Entrar
+              </button>
+            </form>
+          )
+        }
+
+        { isLoading && <Redirect to="/search" /> }
+      </div>
     );
   }
 }
-
 export default Login;
