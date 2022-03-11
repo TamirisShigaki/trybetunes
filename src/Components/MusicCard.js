12af15247538853dd7ec6aea3ade77bb912b7ae0
Tamiris Shigaki <shigakiendo@gmail.com>
2022-03-10 23:17:11 -0300

    requisito 8: ok

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends Component {
-  render() {
-    const { musics } = this.props;
-    return (
-      <div>
-        {
-          musics.map((music) => (
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      checked: [],
+    };
+  }
+
+favoriteMusic = async (event) => {
+  const { musics } = this.props;
+  const favorito = musics
+    .find((objFavorite) => objFavorite.trackId === +event.target.value); // verifica se os id do objeto é igual o id/valor do value, se for retorna meu objeto de music
+  this.setState((preview) => ({
+    loading: true,
+    checked: [...preview.checked, +event.target.value], // mantenho a lista de favoritos e add outra
+  }));
+  await addSong(favorito); // add meu favorito na aPI
+  this.setState({
+    loading: false,
+  });
+}
+
+render() {
+  const { musics } = this.props;
+  const { loading, checked } = this.state;
+  return (
+    <div>
+      {
+        loading ? <Loading />
+          : musics.map((music) => (
             <div key={ music.trackId }>
               <p>{music.trackName}</p>
               <audio data-testid="audio-component" src={ music.previewUrl } controls>
@@ -15,11 +42,25 @@ class MusicCard extends Component {
                 O seu navegador não suporta o elemento
                 <code>audio</code>
               </audio>
+              <br />
+
+              <label htmlFor="favorita">
+                Favorita
+                <input
+                  type="checkbox"
+                  id="favorita"
+                  value={ music.trackId }
+                  data-testid={ `checkbox-music-${music.trackId}` }
+                  checked={ checked.some((id) => id === music.trackId) } // verifico se tem o id dentro do checked e retorna um bolleano, habilitando o botão se for true
+                  onChange={ this.favoriteMusic }
+                />
+              </label>
             </div>))
-        }
-      </div>
-    );
-  }
+      }
+
+    </div>
+  );
+}
 }
 
 MusicCard.propTypes = {
